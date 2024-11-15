9d1671d11125242bf82bf6f43c9935ecede4bb9e
Artem2006Mishin <jock.fromrussia@gmail.com>
2024-11-15 15:22:42 +0300

    Решение с помощью массива символов

---
+++ Lab7-8/Solution with char.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int find_string(char[], char[], char[]);
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	char str[] = "AAB BCA CCB BABC";
+	char str1[] = "AA"; // НЕ содержит
+	char str2[] = "BC"; // содержит
+	
+	int count = find_string(str, str1, str2);
+	if (count != 0)
+	{
+		cout << "Кол-во: " << count;
+	}
+	else
+	{
+		cout << "Слов нет";
+	}
+	
+}
+
+int find_string(char s[], char s1[], char s2[])
+{
+	int count = 0;
+	char* sub, * p = nullptr, * check1, * check2;
+	sub = strtok_s(s, " ", &p);
+
+	while (sub != NULL)
+	{
+		check1 = strstr(sub, s1); 
+		check2 = strstr(sub, s2);
+		if (check1 == NULL && check2 != NULL) {
+			count++;
+		}
+
+		sub = strtok_s(NULL, " ", &p);
+	}
+
+	return count;
+}
