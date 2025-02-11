c03ebc57d40db5914cae83ae008672c64158e8ff
Artem2006Mishin <jock.fromrussia@gmail.com>
2025-02-11 09:39:18 +0300

    Update function.h

--- Lab 2.1/function.h
+++ Lab 2.1/function.h
@@ -5,8 +5,10 @@ class Stack
 public:
 	Stack();
 	Stack(int n, int m);
-	Stack(const Stack&);
+	Stack(const Stack& other);
+	Stack(std::initializer_list<int> arr, int n, int m);
 	~Stack();
+	Stack& operator = (const Stack& other);
 	bool empty(int index);
 	bool full(int index);
 	void push(int index, const int value);
