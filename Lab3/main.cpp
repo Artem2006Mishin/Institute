57a13eb112c48241a0f5efab0b134b2c33fc9b8a
Artem2006Mishin <jock.fromrussia@gmail.com>
2025-02-17 20:23:06 +0300

    Create main.cpp

---
+++ Lab3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <initializer_list>
+#include "function.h"
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    
+    Stack<int> a(3, 3);
+    Stack<int> b(2, 2);
+
+
+
+    return 0;
+}
