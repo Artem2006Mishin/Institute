d436a2018ff36c4bdca472e62efbc1ef3ebc0369
Artem2006Mishin <jock.fromrussia@gmail.com>
2025-02-24 20:01:35 +0300

    Create function.h

---
+++ Lab2.4/function.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+#include <exception>
+
+class StackOverflowException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Stack overflow: Cannot push to a full stack.";
+    }  
+};
+
+class StackUnderflowException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Stack underflow: Cannot pop to an empty stack.";
+    }
+};
+
+class ElementIndexOutOfRangeException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Element index out of range: Invaid element index.";
+    }
+};
+
+class UninitializedMemoryAccessException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Access to uninitialized memory: Element not ininitialized";
+    }
+};
+
+
+class Stack {
+public:
+    Stack();
+    Stack(int n, int m);
+    Stack(const Stack& other);
+    Stack(std::initializer_list<int> arr, int n, int m);
+    ~Stack();
+    Stack& operator = (const Stack& other);
+    friend std::ostream& operator<<(std::ostream& os, const Stack& stack);
+    Stack& operator--();
+    Stack& operator--(int value);
+    bool empty(int index);
+    bool full(int index);
+    void push(int index, const int value);
+    int pop(int index);
+    void print(int index);
+
+    class StackProxy;
+    StackProxy operator[](int index);
+
+private:
+    class StackProxy {
+    public:
+        StackProxy(Stack& stack, int stackIndex);
+        int& operator[](int elementIndex);
+    private:
+        Stack& stack;
+        int stackIndex;
+    };
+
+    int n, m;
+    int* arr;
+    int* top;
+    int* floor;
+};
